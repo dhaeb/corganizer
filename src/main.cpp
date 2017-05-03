c7e94fc794883ac26abfacc883a6fd230f94e315
dhaeb <dan.haeberlein@googlemail.com>
2017-05-03 19:16:09 +0200

    fix bug in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,16 @@
 #include <chrono>
 #include <thread>
 #include <climits>
+#include <future>
 
 using std::this_thread::sleep_for;
 using std::chrono::hours;
-
+using std::promise;
 int main()
 {
-    httpserver_config c("test/resources/html", 8081, 1);
+    httpserver_config c("src/resources/webroot", 8081, 1);
     httpserver server(c);
     server.start();
-    while(true){
-      sleep_for(hours(LLONG_MAX));
-    }
+    std::promise<void>().get_future().wait(); // forever
     return EXIT_SUCCESS;
 }
