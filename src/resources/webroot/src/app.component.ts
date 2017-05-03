1b87bf95372dacec9e936cac47b7eeeb13aa8303
dhaeb <dan.haeberlein@googlemail.com>
2017-05-03 19:18:01 +0200

    initial angular 2 structure

---
+++ src/resources/webroot/src/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="app">
+      hello ingo!
+    </div>
+  `
+})
+export class AppComponent {}
---
+++ src/resources/webroot/src/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component.ts';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ]
+})
+export class AppModule {}
